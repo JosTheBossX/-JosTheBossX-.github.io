591bbd06ffd87b578342ea90cb7c60516c27498d
Yash Kulthe <yashkulthex@gmail.com>
2020-08-29 22:40:47 +0530

    WEBSITE

---
+++ contact/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $country = $_POST['country'];
+    $message = $_POST['message'];
+
+    
+?>
